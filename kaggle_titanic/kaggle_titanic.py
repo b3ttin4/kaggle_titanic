51d87f3afc36a55c43ee2be4bbf09f6fb7820f6c
Bettina <bettina.l.hein@gmail.com>
2021-08-02 18:08:55 -0400

    add keras neural network model to classifier models

--- kaggle_titanic/kaggle_titanic.py
+++ kaggle_titanic/kaggle_titanic.py
@@ -5,8 +5,13 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
 from sklearn.model_selection import GridSearchCV
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 
-def transform_data(data,variables=None):
+import tensorflow as tf
+from tensorflow.keras import layers
+from copy import copy
+
+def transform_data(data_orig):
 	"""
 	Function that takes experimental data and gives us the
 	dependent/independent variables for analysis.
@@ -29,15 +34,17 @@ def transform_data(data,variables=None):
 	y : array
 		label of survival of each passenger
 	"""
-	if isinstance(data, str):
-		data = pd.read_csv(data)
+	if isinstance(data_orig, str):
+		data_orig = pd.read_csv(data_orig)
+
+	data = data_orig
 
 	num_rows,num_variables = data.shape
 	all_columns = data.columns.tolist()
 	clean_data(data,all_columns,ignore_na=False,fill_mode="prob")
 	expand_features(data)
 	variables = ['Pclass','Sex',"Fare","Age","SibSp","Parch","Embarked","Fam_size",\
-				 "cabin_no","ticket_no","friend"]
+				 "cabin_no","ticket_no","friend","Fare_person","Child"]
 	X = pd.get_dummies(data[variables])
 
 	## normalise features to zero man and unit variance
@@ -68,12 +75,25 @@ def expand_features(data):
 	for i,iid in enumerate(data["PassengerId"]):
 		ticket_temp = data.loc[data["PassengerId"]==iid, "ticket_no"].values[0]
 		cabin_temp = data.loc[data["PassengerId"]==iid, "cabin_no"].values[0]
-
 		if data.loc[data["ticket_no"]==ticket_temp, "Ticket"].count()>1:
+			if data.loc[data["PassengerId"]==iid,"Fam_size"].values[0]==0:
+				friends[i] = 1
+		elif cabin_temp!=0:## corresponds to NaN cabin value
 			if data.loc[data["cabin_no"]==cabin_temp, "cabin_no"].count()>1:
 				if data.loc[data["PassengerId"]==iid,"Fam_size"].values[0]==0:
-					friends[i-1] = 1
-		data["friend"] = pd.Series(friends,dtype=int)
+					friends[i] = 1
+	data["friend"] = pd.Series(friends,dtype=int)
+
+	## fare per person
+	fare_per_person = np.zeros((data['PassengerId'].size))
+	for i,ifare in enumerate(data["ticket_no"].unique()):
+		shared_ticket_temp = data.loc[data["ticket_no"]==ifare,"Fare"]
+		fare_per_person[i] = 1.*shared_ticket_temp.values[0]/shared_ticket_temp.count()
+	data["Fare_person"] = pd.Series(fare_per_person,dtype=float)
+
+	## add child feature
+	data.loc[data['Age'] <= 9, 'Child'] = 1
+	data.loc[data['Age'] > 9, 'Child'] = 0
 
 
 def clean_data(data,variables,ignore_na=True,fill_mode="mode"):
@@ -103,7 +123,9 @@ def clean_data(data,variables,ignore_na=True,fill_mode="mode"):
 						data["Age"].fillna(pd.Series(new_age),inplace=True)
 
 					if variable=="Embarked":
-						data[variable] = data[variable].fillna(data[variable].mode()[0])
+						# data[variable] = data[variable].fillna(data[variable].mode()[0])
+						data = data.dropna(axis=0,how="any",subset=["Embarked"])
+						num_rows,num_cols = data.shape
 
 					if variable=="Fare":
 						nan_idx = np.where(data["Fare"].isna())[0]
@@ -131,14 +153,35 @@ def clean_data(data,variables,ignore_na=True,fill_mode="mode"):
 	ticket_no[data['Ticket']=="LINE"] = i + np.arange(np.sum(data['Ticket']=="LINE"))
 	data["ticket_no"] = pd.Series(ticket_no,dtype=int)
 
-	## format cabin values
+	## format cabin values, set NaN cabin values to zero
 	duplicates = []
 	cabin_no = np.zeros((num_rows))
 	for i,uniq in enumerate(data['Cabin'].unique()):
-		cabin_no[data['Cabin']==uniq] = i
+		if uniq=="nan":
+			cabin_no[data['Cabin']==uniq] = 0
+		cabin_no[data['Cabin']==uniq] = i + 1
 	data["cabin_no"] = pd.Series(cabin_no,dtype=int)
-
 	
+
+
+
+
+def build_keras_model(input_shape,optimizer="adam",init="glorot_normal"):
+	model = tf.keras.Sequential()
+	model.add(layers.Dense(8, activation='relu', kernel_initializer=init,\
+						   input_shape=(input_shape,)))#kernel_regularizer=tf.keras.regularizers.L2(0.01)
+	model.add(layers.Dense(16, activation='relu', kernel_initializer=init))
+	model.add(layers.Dropout(0.5))
+	model.add(layers.Dense(16, activation='relu', kernel_initializer=init))
+	model.add(layers.Dropout(0.5))
+	model.add(layers.Dense(1, activation='sigmoid'))
+
+	# compile model
+	model.compile(optimizer=optimizer,
+					loss='binary_crossentropy',	#'mse' 'binary_crossentropy'
+					metrics=['accuracy'])		#'mae'	'categorical_accuracy'
+					
+	return model
 
 
 class Model(object):
@@ -210,8 +253,32 @@ class Model(object):
 			score = clf.score(x,y)
 			params = {}#{"params" : grid_result.best_params_}
 
+		elif self.classific_method=="NeuralNetwork":
+			seed = 7
+			np.random.seed(seed)
+			input_shape = x.shape[1]
+
+
+			clf = build_keras_model(input_shape,optimizer="adam",init="glorot_normal")
+
+			n_epochs = 200
+			n_sub_epochs = 10
+			sub_epoch_size = len(x) // n_sub_epochs
+			# for epoch_number in range(50):
+			# 	for sub_epoch in range(n_sub_epochs):
+			# 		X = x[sub_epoch * sub_epoch_size: (sub_epoch + 1) * sub_epoch_size]
+			# 		Y = y[sub_epoch * sub_epoch_size: (sub_epoch + 1) * sub_epoch_size]
+			# 		hist = clf.fit(X,Y,epochs=1);
+			hist=clf.fit(x, y, epochs=n_epochs, batch_size=sub_epoch_size, verbose=0)
+			acc = hist.history['accuracy']
+			loss = hist.history['loss']
+			score = acc[-1]
+			params = {"acc" : acc, "loss" : loss}
+
 		return clf, score, params
 
 
 
 
+if __name__=="__main__":
+	print("pass")
